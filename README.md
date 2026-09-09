80e7548d8dfdd3c4a57efbdcfb67140349146623
ummita <maxmora401@gmail.com>
2026-09-09 00:47:41 -0300

    feat: se agrega boton y el readme

---
+++ README.md
@@ -0,0 +1,13 @@
+![Logo Ojo Ciudadano](https://raw.githubusercontent.com/diegoa18/OjoCiudadanoAndroid/main/logo.png)
+
+# Ojo Ciudadano - Aplicación Android
+
+Aplicación móvil del proyecto **Ojo Ciudadano**, desarrollada en **Android Studio** con **Kotlin**.
+
+
+## Descripción
+
+**Ojo Ciudadano** es una aplicación móvil orientada al reporte y seguimiento de incidencias ciudadanas.
+
+Este repositorio contiene exclusivamente el desarrollo de la aplicación móvil para Android.
+
--- app/src/main/java/cl/uct/ojociudadano/MainActivity.kt
+++ app/src/main/java/cl/uct/ojociudadano/MainActivity.kt
@@ -4,8 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -20,10 +23,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             OjoCiudadanoTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    MainScreen(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
@@ -31,17 +31,22 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MainScreen(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = androidx.compose.ui.Alignment.CenterHorizontally
+    ) {
+        Button(onClick = { }) {
+            Text("Boton")
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun MainScreenPreview() {
     OjoCiudadanoTheme {
-        Greeting("Android")
+        MainScreen()
     }
 }
